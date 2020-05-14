d52ff7f8a34b2977dd534cfabd4b41d74845cf46
dzkjz <dzkjzdzkjz@gmail.com>
2020-05-14 20:30:27 +0800

    Project - Laravel API with Nuxt JS frontend client

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container col-md-6 mt-5">
+    <h2>Login</h2>
+    <br>
+    <form>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+    <br>
+    <p>Don't have an account?
+      <nuxt-link to="/register">Register</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "login"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container col-md-6 mt-5">
+    <h2>Register</h2>
+    <br>
+    <form>
+      <div class="form-group">
+        <label>First Name</label>
+        <input type="text" class="form-control" placeholder="Enter your first name">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Last Name</label>
+        <input type="text" class="form-control" placeholder="Enter your last name">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Email address</label>
+        <input type="email" class="form-control" placeholder="Enter your email address">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Password</label>
+        <input type="password" class="form-control" placeholder="Enter your password">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <div class="form-group">
+        <label>Confirm Password</label>
+        <input type="password" class="form-control" placeholder="Confirm your password">
+        <small class="form-text text-danger">Show errors here</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Register</button>
+    </form>
+    <br>
+    <p>Already have an account?
+      <nuxt-link to="/login">Login</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "register"
+  }
+</script>
+
+<style scoped>
+
+</style>
